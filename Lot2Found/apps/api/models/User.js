ca1ee8712565bcb51000ab2eefc28cf12278c0c1
Sajana Wickramarathna <sajanawickramarathna143@gmail.com>
2026-06-15 12:19:46 +0530

    Update User.js

--- Lot2Found/apps/api/models/User.js
+++ Lot2Found/apps/api/models/User.js
@@ -43,9 +43,9 @@ userSchema.virtual('isLocked').get(function() {
 });
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function () {
   if (!this.isModified('password')) {
-    return next();
+    return;
   }
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
